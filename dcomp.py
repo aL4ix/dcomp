a937a9b337e00106231cd1508cfe5b4612d0de46
Alan Lima <alan.limagalvez@yahoo.com>
2025-01-19 21:56:21 -0600

    Improving the matching to handle cases were the filters give more than one match

--- dcomp.py
+++ dcomp.py
@@ -32,7 +32,11 @@ class Stats:
 
     def get_checksum(self):
         if self.checksum == 1:
-            self.checksum = calculate_checksum(self.get_complete_file_path())
+            print(f'GENERATING checksum for {self.get_complete_file_path()}')
+            if self.mode == FileMode.DIR:
+                print('WARNING: ISDIR!')
+            else:
+                self.checksum = calculate_checksum(self.get_complete_file_path())
         return self.checksum
 
     def get_complete_file_path(self):
@@ -45,7 +49,6 @@ def walk(root: str, results: list[Stats], extractor: Callable[[pathlib.Path, str
 
     for file in pathlib.Path(root).iterdir():
         leaf, path = extractor(file, ori_root)
-        print(file)
         stats = file.stat()
         stat_mode = stats.st_mode
         if stat.S_ISREG(stat_mode):
@@ -75,81 +78,103 @@ def calculate_checksum(file, chunk_size=65536):
     return checksum
 
 
-def algo(node_a: Stats, m: Stats, list_b: list[Stats], comparator: Callable[[Stats, Stats], int], merged: list[Stats]):
-    same = False
-    if m.mode == node_a.mode:
-        if m.mode == FileMode.DIR:
-            same = True
-
-        if m.mode == FileMode.FILE and m.size == node_a.size:
-            if m.mtime == node_a.mtime:
-                same = True
-            else:
-                if m.get_checksum() == node_a.get_checksum():
-                    same = True
-    list_b.remove(m)
-    if not same:
-        print(f'DECISION: "{m.leaf}"')
-        m.decision = True
-        node_a.decision = True
-        keep_a = comparator(node_a, m)
-        if keep_a > 0:
-            m.keep = False
-        elif keep_a < 0:
-            node_a.keep = False
-        merged.append(m)
-    else:
-        node_a.decision = False
-        m.decision = False
-        m.keep = False
-
-
 def filter_matches(list_to_filter: list[Stats], fil_func: Callable) -> tuple[int, list[Stats], Stats]:
+    """
+    Take a list of Stats and apply fil_func to filter it.
+
+    :param list_to_filter: the original list to be used to iterate and filter
+    :param fil_func: function used to filter
+    :return: a tuple of (length, a list of matches, and the first match if any)
+    """
     matches = list(filter(fil_func, list_to_filter))
     length = len(matches)
-    m = None
-    if length > 0:
-        m = matches[0]
+    m = matches[0] if length > 0 else None
     return length, matches, m
 
 
-def get_match(node_a: Stats, list_a: list[Stats], list_b: list[Stats]):
+def try_to_find_one_match_or_closest(mode: FileMode, closest_match_vs_exact: bool, list_: list[Stats], *args) -> tuple[
+    int, list[Stats], Stats]:
+    prev_length = len(list_)
+    prev_matches = list_
+    prev_m = list_[0] if prev_length > 0 else None
+    for index, fil_func in enumerate(args):
+        length, matches, m = filter_matches(prev_matches, fil_func)
+        if length > 0:
+            print(f'Try to find one match, iter {index} found {length} matches')
+        if ((length == 1 and closest_match_vs_exact)
+            or (mode == FileMode.DIR and index >= 1)
+            or (length == 0 and index < 2)
+            or (length == 0 and not closest_match_vs_exact)):
+            return length, matches, m
+        elif length == 0:
+            break
+        else:
+            print('next')
+            prev_length = length
+            prev_matches = matches
+            prev_m = m
+    return prev_length, prev_matches, prev_m
+
+
+def get_match(node_a: Stats, list_a: list[Stats], list_b: list[Stats], merged, to_delete, comparator: Callable):
     # Maybe there is more than one match from a to b or the other way
     # Check if this node's name matches only once from both sides, if not get lists from both, a and b, and look for the
     # exact matches and discard them, then get the first in the list, if any
-    length_m, matches_m, m = filter_matches(list_b, lambda x: x.leaf == node_a.leaf)
-    if m:  # At least one m
-        length_a, matches_a, a = filter_matches(list_a, lambda x: x.leaf == m.leaf)  # Check matches for a with first m
-        if length_a == 1:  # Maybe many m but first m has only one match
-            return m
+    mode = node_a.mode
+    length_m, matches_m, first_m = try_to_find_one_match_or_closest(mode, True, list_b,
+                                                                    lambda b: b.leaf == node_a.leaf,
+                                                                    lambda b: b.mode == node_a.mode,
+                                                                    lambda b: b.size == node_a.size,
+                                                                    lambda b: b.get_checksum() == node_a.get_checksum())
+    matches_a = []
+    if length_m > 0:
+        for index, m in enumerate(matches_m):
+            print(f'Find b in a iter={index}')
+            length_a, matches_a, new_a = try_to_find_one_match_or_closest(mode, False, list_a,
+                                                                          lambda a: a.leaf == m.leaf,
+                                                                          lambda a: a.mode == m.mode,
+                                                                          lambda a: a.size == m.size,
+                                                                          lambda a: a.get_checksum() == m.get_checksum())
+            if node_a in matches_a and not(mode == FileMode.FILE and length_a != 1):  # Found the one
+                print('found the one')
+                list_b.remove(m)
+                to_delete.append(m)
+                node_a.decision = False
+                m.decision = False
+                m.keep = False
+                return m
 
-        # Many to many
-
-        # Quit
-        for match_m in matches_m:
-            match_m.decision = True
+    # Quit
+    if matches_m:
+        print(f'DECISION:')
         for match_a in matches_a:
+            print(f' A {match_a.get_complete_file_path()} {match_a.get_checksum()}')
             match_a.decision = True
-        m = None
-        # length, matches, m = filter_matches(lambda x: x.size == node_a.size, node_a, matches_m)
-        # if length > 1:
-        #     length, matches, m = filter_matches(lambda x: x.get_checksum() == node_a.get_checksum(), node_a, matches)
-        #     if length > 1:
-        #         m = matches[0]
-    return m
+        for match_m in matches_m:
+            print(f' M {match_m.get_complete_file_path()} {match_m.get_checksum()}')
+            match_m.decision = True
+            match_m.keep = False
+        # FIXME keep algo can be improved
+        all_aprox = [node_a]
+        all_aprox.extend(matches_m)
+        all_aprox.sort(key=lambda x: x.size)
+        all_aprox[0].keep = True
+    return None
 
 
 def compare_two_lists(list_a: list[Stats], list_b: list[Stats], comparator: Callable[[Stats, Stats], int]) \
-        -> list[Stats]:
+        -> tuple[list[Stats], list[Stats]]:
     merged = []
+    to_delete = []
     for node_a in list_a:
-        m = get_match(node_a, list_a, list_b)
+        print(node_a.get_complete_file_path())
+        get_match(node_a, list_a, list_b, merged, to_delete, comparator)
         merged.append(node_a)
-        if m:
-            algo(node_a, m, list_b, comparator, merged)
 
     merged.extend(list_b)
-    return merged
+    print('TO_DELETE')
+    print('\n'.join(repr((d.get_complete_file_path(), d.get_checksum())) for d in to_delete))
+    return merged, to_delete
 
 
 def compare_within_one_list(l: list[Stats], comparator: Callable[[Stats, Stats], int]) -> list[Stats]:
@@ -159,7 +184,7 @@ def compare_within_one_list(l: list[Stats], comparator: Callable[[Stats, Stats],
             if node is not node2:
                 if node.leaf == node2.leaf:
                     merged.append(node)
-                    algo(node, node2, l, comparator, merged)
+                    # FIXME No algo yet
     merged.extend(l)
     return merged
 
@@ -175,7 +200,9 @@ def compare_two_folders(a: str, b: str, extractor: Callable[[pathlib.Path, str],
     list_b = []
     walk(a, list_a, extractor)
     walk(b, list_b, extractor)
-    merged = compare_two_lists(list_a, list_b, comparator)
+    print(f'WALK: {len(list_a)+len(list_b)}')
+    merged, to_delete = compare_two_lists(list_a, list_b, comparator)
+    print(len(merged)+len(to_delete))
     create_report(merged)
 
 
@@ -207,9 +234,9 @@ def extract_with_complete_path(file: pathlib.Path, ori_root: str):
 
 
 def main():
-    a = '/home/a'
-    b = '/home/b'
-    compare_two_folders(a, b, extract_only_caring_about_filename, keep_bigger)
+    path_a = '/home/a'
+    path_b = '/home/b'
+    compare_two_folders(path_a, path_b, extract_only_caring_about_filename, keep_bigger)
 
 
 if __name__ == '__main__':
